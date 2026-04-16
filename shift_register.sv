159c6d632eb52e28c0c5f840d6f7db93482d01a9
AmazingChris520 <chrisant520@gmail.com>
2026-04-16 09:27:03 -0700

    Added shift register

---
+++ shift_register.sv
@@ -0,0 +1,24 @@
+module shift_register
+(
+    input  logic clk,
+    input  logic rst,
+    input  logic data,
+    input  logic wr_en,
+    output logic [31:0] out
+);
+
+parameter DIRECTION = 0;
+
+always @(posedge clk or posedge rst) begin
+    if (rst) begin
+        out <= 33'b0;
+    end
+    else if (wr_en) begin
+        if (DIRECTION)
+            out <= {data, out[31:1]};
+        else
+            out <= {out[30:0], data};
+    end
+end
+
+endmodule
